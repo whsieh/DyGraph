0a6ccc73f6f0da2d55a3d1a31105b2f05f2a3ee9
whsieh <whsieh256@gmail.com>
2012-12-19 22:19:45 -0800

    Update src/example/AlchemyQueryExample.java

--- src/example/AlchemyQueryExample.java
+++ src/example/AlchemyQueryExample.java
@@ -11,7 +11,7 @@ import java.net.URLEncoder;
 
 public class AlchemyQueryExample {
 
-	final static public String API_KEY = "d9c63c6528360164c610975a091d4eda26d55be5";
+	final static public String API_KEY = "[REDACTED]";
 	final static public String ENDPOINT = "http://access.alchemyapi.com/calls/text/TextGetRankedKeywords";
 	final static public String PARAMS = "?apikey=" + API_KEY + "&keywordExtractMode=strict&text=";
 	
